a6f7da585f2aa23f976e3d33f70ca178b21db167
Suryansh Singh <80690023+suryanshsingh2001@users.noreply.github.com>
2023-11-06 19:04:25 +0530

    Impove Layout

--- src/Components/Layout.jsx
+++ src/Components/Layout.jsx
@@ -7,15 +7,26 @@ const Layout = ({ photos }) => {
 
   return (
     <div className="container mx-auto my-8">
-      <h2 className="text-2xl font-bold mb-4">Search Results</h2>
-      <div className="grid grid-cols-3 gap-4">
+      <h2 className="text-3xl font-bold text-center mb-8">Search Results</h2>
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
         {photos.map((photo) => (
           <div key={photo.id} className="bg-white rounded-lg overflow-hidden shadow-lg">
             <img
               src={`https://live.staticflickr.com/${photo.server}/${photo.id}_${photo.secret}_q.jpg`}
-              alt={photo.title}
-              className="w-full"
+              alt={photo.title || "Unknown"}
+              className="w-full h-64 object-cover"
             />
+            <div className="p-6">
+              <h3 className="text-xl font-semibold mb-2">{photo.title || "Unknown"}</h3>
+              <a
+                href={`https://www.flickr.com/photos/${photo.owner}/${photo.id}`}
+                target="_blank"
+                rel="noopener noreferrer"
+                className="block text-blue-500 font-semibold mt-4 hover:underline"
+              >
+                View on Flickr
+              </a>
+            </div>
           </div>
         ))}
       </div>
